9f39a4615e1fdbd1fa5592371e8b87159cfe75f0
Alan Fidelino <afidedev26@gmail.com>
2016-08-30 21:56:08 +1000

    3.8 Static Pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def landing_page
+  end
+
+  def thank_you
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'index' => 'static_pages#index'
+
+  get 'about' => 'static_pages#contact'
+
+  get 'faq'   => 'static_pages#faq'
+
+  root to:'static_pages#landing_page'
+
+  #get 'static_pages/thank_you'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
