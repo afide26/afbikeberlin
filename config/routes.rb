50cb802f3834731e9784a4362b12dddefce5f16e
Alan Fidelino <afidedev26@gmail.com>
2016-08-31 20:01:37 +1000

    Database migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   resources :products
-  get 'index' => 'static_pages#index'
 
-  get 'about' => 'static_pages#contact'
+  get 'contact' => 'static_pages#contact'
 
-  get 'faq'   => 'static_pages#faq'
+  get 'faq'     => 'static_pages#faq'
 
-  root to:'static_pages#landing_page'
-
-  #get 'static_pages/thank_you'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#index'
 end
