827ed8fbbb1ebc8482d58ad27a68026f4665be2a
Alan Fidelino <afidedev26@gmail.com>
2016-09-04 17:44:58 +1000

    6.3 Functional tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,14 +6,16 @@ describe Comment do
    before do
     @user = User.create!(email:"alan@example.com", password:"
       alanfidelino")
-    @product = Product.create!(name:"BMX Bike")
-
-    @product.comments.create!(rating:"", user:@user, body:
-      "Test comment")
+    @product = Product.create!(name:"BMX Bike", price: 200)
    end
 
    it "should not be valid without a rating" do
-    expect(@product.comments.rating=="").not_to be_valid
+    expect(Comment.new(body:"Awesome bicycle", user:@user, rating:0)).to_not be_valid
+   end
+
+   it "should have a body with a minimum of 10 characters" do
+    @comment = @product.comments.create(body:"Awesome bicycle", user:@user, rating:0)
+    expect(@comment.body.length >= 10).to be_truthy
    end
   end
 end
