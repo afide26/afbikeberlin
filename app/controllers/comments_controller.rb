8efdf1059fee11597c30210cee4ee72ff0285ea0
Alan Fidelino <afidedev26@gmail.com>
2016-09-07 21:54:10 +1000

    6.10 AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html{redirect_to @product, success: 'Comment was created successfully'}
         format.json{render :show, status: :created, location: @product}
+        format.js
       else
         format.html{redirect_to @product, warning: 'Your comment was not saved.'}
         format.json{render json: @comment.errors, status: :unprocessable_entity}
@@ -20,6 +21,7 @@ class CommentsController < ApplicationController
     product = @comment.product
 
     @comment.destroy
+    flash[:danger] = "Comment deleted."
     redirect_to product
   end
 
