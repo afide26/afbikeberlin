1095bdfdd570c9a3557fa2e6ebd4ab6c03b6a7b0
Alan Fidelino <afidedev26@gmail.com>
2016-09-10 22:10:23 +1000

    6.14 ActiveJob and Advanced ActionCable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     @comment.user = current_user
     respond_to do |format|
       if @comment.save
-        ActionCable.server.broadcast 'product_channel', comment: @comment
+        # ActionCable.server.broadcast 'product_channel', comment: @comment
         format.html{redirect_to @product, success: 'Comment was created successfully'}
         format.json{render :show, status: :created, location: @product}
         format.js
