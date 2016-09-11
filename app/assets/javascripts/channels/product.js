18c62bb54ccb7c9dfc7d8a703269f5413c5d478c
Alan Fidelino <afidedev26@gmail.com>
2016-09-11 14:54:49 +1000

    Change to README

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,4 +1,4 @@
-App.product = App.cable.subscriptions.create("ProductChannel", {
+App.product = App.cable.subscriptions.create({channel:"ProductChannel"}, {
   connected: function() {
     // Called when the subscription is ready for use on the server
   },
