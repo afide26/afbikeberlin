02a6a4485dc858c11c07626752ef05a0b1bccb88
Alan Fidelino <afidedev26@gmail.com>
2016-09-14 21:50:52 +1000

    6.14 Add action cable metatag

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_cable.url = "wss://cfbikeberlinapp.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://cfbikeberlinapp.herokuapp.com','https://cfbikeberlinapp.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://cfbikeberlinapp.herokuapp.com','http://cfbikeberlinapp.herokuapp.com']
 end
